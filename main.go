9606aaa90cd0397a126da2e5c079e072f78ecc1d
Rohan Shukla <rdshukla1994@gmail.com>
2023-12-17 00:58:52 +0530

    Read input file and print on screen

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"flag"
+	"os"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 func main() {
 	fmt.Println("hi")
+
+	var inputFile string
+	flag.StringVar(&inputFile, "file", "test.bhai", "The input file to be compiled")
+
+	flag.Parse()
+
+	fmt.Printf("Input File: %s\n", inputFile)
+
+	dat, err := os.ReadFile(inputFile)
+    check(err)
+    fmt.Print(string(dat))
 }
+
+// func parseFile() {
+
+// }
